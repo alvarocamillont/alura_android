831cd91596e28dcad172ef3027fe6321ec0bd989
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2023-09-22 23:08:02 -0300

    Criando o layout

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -2,11 +2,10 @@ package com.example.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 
 class MainActivity: Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Seja Bem Vindo", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 }
