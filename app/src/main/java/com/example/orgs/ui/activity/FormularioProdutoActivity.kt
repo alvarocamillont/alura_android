a002981792b78d8e4e9af357b8f622574838a113
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2023-09-23 14:43:48 -0300

    Definição do formulário

--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,8 +2,23 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.example.orgs.R
 
 class FormularioProdutoActivity :
     AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            Log.i("FormularioProduto", "onCreate: $nome")
+        }
+    }
 }
