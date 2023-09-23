4b5d3ac3638661a53bee45a06c8fbd40310d6a1f
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2023-09-23 15:15:38 -0300

    Criação do floating button

--- app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.orgs.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.example.orgs.R
 import com.example.orgs.model.Produto
 import com.example.orgs.ui.recycleview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity: AppCompatActivity(R.layout.activity_main){
@@ -21,5 +23,12 @@ class MainActivity: AppCompatActivity(R.layout.activity_main){
             )
         )
 
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
+
